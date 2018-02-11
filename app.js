00d9ee238904bb1755423e6344ea6bb1e9495e64
SushmithaPatlolla <sushmithareddy42@gmail.com>
2018-02-11 00:23:58 -0500

    Todo commit

---
+++ app.js
@@ -0,0 +1,21 @@
+$('ul').on('click', 'li', function(){
+    $(this).toggleClass("completed")
+})
+
+$('ul').on('click', 'span', function(){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    })
+ event.stopPropagation();
+
+})
+$('input').keypress(function(event){
+if(event.which === 13){
+    var list = $(this).val();
+    $(this).val("")
+    $('ul').append("<li><span><i class='fa fa-trash'></i></span> "+ list + "</li>")
+}
+})
+$('.fa-plus').click(function(){
+    $('input').fadeToggle()
+})
